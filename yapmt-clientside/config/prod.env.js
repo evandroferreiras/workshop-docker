22cd351a429f97d473ac872dbfd3e5a84b02056d
Evandro Souza <evandro.souza@corp.globo.com>
2019-10-18 19:40:49 -0300

    fix: changed to localhost

--- yapmt-clientside/config/prod.env.js
+++ yapmt-clientside/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://' + process.env.API_URL + ':9000"' //Docker-machine windows
+  API_URL: '"http://localhost:9000"'
 }
